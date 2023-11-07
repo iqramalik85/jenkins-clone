4491c4b9de726328c6b6d64f0fda137aa3a5e429
iqramalik85 <140602763+iqramalik85@users.noreply.github.com>
2023-11-07 15:59:31 +0500

    Update and rename uel.sh to url.sh

--- url.sh
+++ url.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
+url=www.google.com
 
-url="$1"
-
-while read -r url
-do
 status=$(curl -o /dev/null -s -w "%{http_code}" "$url")
 
 if [ $status = 200 ]
-
 then
     echo "$url This URL is working and Status Code is: $status"
 else
     echo "$url This URL is not working and Status Code is: $status"
 fi
-done < $url
